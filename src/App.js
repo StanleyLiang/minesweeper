f88cc4c91b9e40d23de0952694b9db6201f33181
Stanley Liang <stanley.liang@amazingtalker.com>
2023-05-08 22:46:18 +0800

    fix: remove unused imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState } from "react";
 import classNames from "classnames";
 import Col from "./Col";
 import { initBoard, placeMines, openCol, flagCol } from "./utils";
